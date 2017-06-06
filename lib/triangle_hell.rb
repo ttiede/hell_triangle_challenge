3c2d367bd0f0effada53b144ada6e43ddeb8d85d
Tiago Tiede <tiago@navut.com>
2017-06-06 19:56:43 -0300

    Refactored Test

--- lib/triangle_hell.rb
+++ lib/triangle_hell.rb
@@ -1,15 +1,12 @@
 class TriangleHell
   def self.maximum_total(tree, index = 0)
-    begin
     row = tree.shift
 
     max_index = row.length == 1 || row[index] >= row[index + 1] ? index : index + 1
-    
+
     return row[max_index] if tree.empty?
     return row[max_index] + maximum_total(tree, max_index) unless tree.empty?
-
-    rescue Exception => e
-      puts "#{e}"
-    end
+  rescue => e
+    puts e.to_s
   end
 end
